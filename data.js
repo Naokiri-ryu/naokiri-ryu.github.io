f6c9b31ffe9492f699f712391732d5fb9fec21af
Naokiri-ryu <zmagichunter@gmail.com>
2022-08-25 00:16:32 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,36 @@
+// const nama = "Alvaro Sinaga";
+// let usia = 16;
+
+// let biodata = document.getElementById("biodata")
+// console.log(biodata);
+
+// function generateBiodata() {
+//     let golongan;
+
+//     if (usia >= 12 && usia <= 18) {
+//         golongan = "golongan remaja";
+//     }
+//     else if (usia > 18 && usia < 50) {
+//         console.log("anda dewasa")
+//         golongan = "golongan dewasa"
+//     }
+//     else if (usia >= 0 && usia < 12) {
+//         console.log("anda anak-anak")
+//         golongan = "golongan anak-anak"
+//     }
+//     else if (usia >= 50) {
+//         console.log("anda lansia")
+//         golongan = "golongan lansia"
+//     }
+//     else {
+//         console.log("lu itu apa")
+//     }
+
+//     return biodata.innerHTML = golongan
+
+
+// }
+
+
+
+// generateBiodata();
